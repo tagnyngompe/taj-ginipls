7c67babb5300f8846f5840c2e650c19d15fd48a2
tagny <tagny@ymail.com>
2020-05-16 20:25:03 +0200

    back in biz

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='A classifier based on a combination of the Gini-PLS and Logit-PLS methods.',
-    author='tagny',
-    license='MIT',
+    author='',
+    license='',
 )
